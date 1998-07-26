f61b7cdee1b7fa3af8274a7554517ec53dc24c73
James P. Howard, II <jh@jameshoward.us>
1998-07-26 17:02:38 -0500

    This is an original implementation of the daemon program available on m-net.arbornet.org, which is useful for starting programs and ensuring they are not killed by terminal disconnects.
    Signed-off-by: James P. Howard, II <jh@jameshoward.us>

---
+++ daemon.c
@@ -0,0 +1,42 @@
+/*
+ *  I saw a similar program on a BSD/OS 3.1 system.  Used it to exploit
+ *  local security holes.
+ */ 
+
+#include <err.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+
+static void help();
+
+int main(int argc, char *argv[]) 
+{
+	int c, nochdir = 1, noclose = 1;
+
+	if(argc < 2)
+		help();
+	while((c = getopt(argc, argv, "cf")) != EOF)
+		switch(c) {
+		case 'c':
+			nochdir = 0;
+			break;
+		case 'f':
+			noclose = 0;
+			break;
+		default:
+			help();
+		}
+	argc -= optind;
+        argv += optind;  
+	if (daemon(nochdir, noclose) == -1)
+		err(1, "");
+	execvp(argv[0], &argv[0]);
+	err(1, "%s", argv[0]);
+}
+
+void help() {
+
+	(void)fprintf(stderr, "\
+usage: daemon [-cf] executable argv[1] ...\n");
+	(void)exit(2);
+}
